c07509613576d7d104cdb7de5e5bedea9d86be85
Peter Kormos <peter@baxter-it.com>
2014-11-28 10:40:23 +0100

    Elasticsearch log4j appenders added.

---
+++ src/main/java/com/baxter/log4j/appender/impl/jest/ESAppender.java
@@ -0,0 +1,247 @@
+package com.baxter.log4j.appender.impl.jest;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.ClientConfig;
+import io.searchbox.core.Bulk;
+import io.searchbox.core.Bulk.Builder;
+import io.searchbox.core.Index;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+
+public class ESAppender extends AppenderSkeleton
+{
+  private final ReentrantLock lock = new ReentrantLock();
+  List<LoggingEvent> events = new LinkedList<LoggingEvent>();
+  final Semaphore semaphore = new Semaphore(1);
+
+  private JestClient client;
+
+  WorkerThread thread = new WorkerThread();
+
+  private String host;
+  private int port;
+  private String clusterName;
+  private String index, type;
+  private int queuingWarningLevel;
+
+  public ESAppender()
+  {
+  }
+
+  class WorkerThread extends Thread
+  {
+	boolean isRunning = true;
+
+	@Override
+	public void run()
+	{
+	  while (isRunning)
+	  {
+		try
+		{
+		  semaphore.acquire();
+		  lock.lock();
+
+		  List<LoggingEvent> currentEvents = null;
+		  try
+		  {
+			currentEvents = events;
+			events = new LinkedList<LoggingEvent>();
+		  }
+		  finally
+		  {
+			lock.unlock();
+		  }
+
+		  if (!currentEvents.isEmpty())
+		  {
+			processEvents(currentEvents);
+		  }
+		}
+		catch (final Exception e)
+		{
+		  e.printStackTrace();
+		}
+	  }
+	}
+
+	private void processEvents(List<LoggingEvent> currentEvents) throws Exception
+	{
+	  final Builder bulk = new Bulk.Builder();
+
+	  for (final LoggingEvent loggingEvent : currentEvents)
+	  {
+		final Map<String, Object> data = new HashMap<String, Object>();
+
+		writeBasic(data, loggingEvent);
+		writeThrowable(data, loggingEvent);
+
+		bulk.addAction(new Index.Builder(data).index(index).type(type).build());
+	  }
+
+	  if (currentEvents.size() > queuingWarningLevel)
+	  {
+		final long startTime = System.currentTimeMillis();
+		client.execute(bulk.build());
+		System.err.printf("currentEvents.size(): %d$1 queuingWarningLevel: %d$2 processTime: %d$3\n", currentEvents.size(),
+		    queuingWarningLevel, (System.currentTimeMillis() - startTime));
+	  }
+	  else
+	  {
+		client.execute(bulk.build());
+	  }
+	}
+
+	protected String getStackTrace(Throwable aThrowable)
+	{
+	  final Writer result = new StringWriter();
+	  final PrintWriter printWriter = new PrintWriter(result);
+	  aThrowable.printStackTrace(printWriter);
+	  return result.toString();
+	}
+
+	protected void writeBasic(Map<String, Object> json, LoggingEvent event)
+	{
+	  json.put("hostName", getHost());
+	  json.put("timestamp", event.getTimeStamp());
+	  json.put("logger", event.getLoggerName());
+	  json.put("level", event.getLevel().toString());
+	  json.put("message", event.getMessage());
+	}
+
+	protected void writeThrowable(Map<String, Object> json, LoggingEvent event)
+	{
+	  final ThrowableInformation ti = event.getThrowableInformation();
+	  if (ti != null)
+	  {
+		final Throwable t = ti.getThrowable();
+
+		json.put("className", t.getClass().getCanonicalName());
+		json.put("stackTrace", getStackTrace(t));
+	  }
+	}
+  }
+
+  @Override
+  public void close()
+  {
+	client.shutdownClient();
+	events.clear();
+	thread.isRunning = false;
+	semaphore.release();
+  }
+
+  @Override
+  public boolean requiresLayout()
+  {
+	return false;
+  }
+
+  @Override
+  protected void append(LoggingEvent event)
+  {
+	lock.lock();
+	try
+	{
+	  events.add(event);
+
+	  semaphore.release();
+	}
+	finally
+	{
+	  lock.unlock();
+	}
+  }
+
+  @Override
+  public void activateOptions()
+  {
+	super.activateOptions();
+
+	final JestClientFactory factory = new JestClientFactory();
+	factory.setClientConfig(new ClientConfig.Builder("http://" + host + ":" + port).multiThreaded(true).build());
+	client = factory.getObject();
+
+	thread.start();
+  }
+
+  public String getHost()
+  {
+	return host;
+  }
+
+  public void setHost(String host)
+  {
+	this.host = host;
+  }
+
+  public int getPort()
+  {
+	return port;
+  }
+
+  public void setPort(int port)
+  {
+	this.port = port;
+  }
+
+  public String getClusterName()
+  {
+	return clusterName;
+  }
+
+  public void setClusterName(String clusterName)
+  {
+	this.clusterName = clusterName;
+  }
+
+  public String getIndex()
+  {
+	return index;
+  }
+
+  public void setIndex(String index)
+  {
+	this.index = index;
+  }
+
+  public String getType()
+  {
+	return type;
+  }
+
+  public void setType(String type)
+  {
+	this.type = type;
+  }
+
+  public int getQueuingWarningLevel()
+  {
+	return queuingWarningLevel;
+  }
+
+  public void setQueuingWarningLevel(int queuingWarningLevel)
+  {
+	this.queuingWarningLevel = queuingWarningLevel;
+  }
+
+  @Override
+  public String toString()
+  {
+	return "ESAppender [host=" + host + ", port=" + port + ", clusterName=" + clusterName + ", index=" + index + ", type=" + type
+	    + ", queuingWarningLevel=" + queuingWarningLevel + "]";
+  }
+}
---
+++ src/main/java/com/baxter/log4j/appender/impl/transportclient/ESAppender.java
@@ -0,0 +1,253 @@
+package com.baxter.log4j.appender.impl.transportclient;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+public class ESAppender extends AppenderSkeleton
+{
+  private final ReentrantLock lock = new ReentrantLock();
+  List<LoggingEvent> events = new LinkedList<LoggingEvent>();
+  final Semaphore semaphore = new Semaphore(1);
+
+  private TransportClient client;
+
+  WorkerThread thread = new WorkerThread();
+
+  private String host;
+  private int port;
+  private String clusterName;
+  private String index, type;
+  private int queuingWarningLevel;
+
+  public ESAppender()
+  {
+  }
+
+  class WorkerThread extends Thread
+  {
+	boolean isRunning = true;
+
+	@Override
+	public void run()
+	{
+	  while (isRunning)
+	  {
+		try
+		{
+		  semaphore.acquire();
+		  lock.lock();
+
+		  List<LoggingEvent> currentEvents = null;
+		  try
+		  {
+			currentEvents = events;
+			events = new LinkedList<LoggingEvent>();
+		  }
+		  finally
+		  {
+			lock.unlock();
+		  }
+
+		  if (!currentEvents.isEmpty())
+		  {
+			processEvents(currentEvents);
+		  }
+		}
+		catch (final Exception e)
+		{
+		  e.printStackTrace();
+		}
+	  }
+	}
+
+	private void processEvents(List<LoggingEvent> currentEvents) throws Exception
+	{
+	  final BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+	  for (final LoggingEvent loggingEvent : currentEvents)
+	  {
+		final XContentBuilder source = jsonBuilder().startObject();
+		source.field("timestamp", loggingEvent.getTimeStamp());
+		source.field("message", loggingEvent.getMessage());
+		source.field("logger", loggingEvent.getLoggerName());
+		source.field("level", loggingEvent.getLevel().toString());
+
+		final ThrowableInformation throwableInformation = loggingEvent.getThrowableInformation();
+		if (throwableInformation != null)
+		{
+		  final Throwable throwable = throwableInformation.getThrowable();
+
+		  source.field("className", throwable.getClass().getCanonicalName());
+		  source.field("stackTrace", getStackTrace(throwable));
+		}
+
+		bulkRequest.add(client.prepareIndex(index, type).setSource(source.endObject()));
+	  }
+
+	  if (currentEvents.size() > queuingWarningLevel)
+	  {
+		final long startTime = System.currentTimeMillis();
+		final BulkResponse bulkResponse = bulkRequest.execute().actionGet();
+		System.err.printf("currentEvents.size(): %d$1 queuingWarningLevel: %d$2 processTime: %d$3\n", currentEvents.size(),
+		    queuingWarningLevel, (System.currentTimeMillis() - startTime));
+	  }
+	  else
+	  {
+		final BulkResponse bulkResponse = bulkRequest.execute().actionGet();
+	  }
+	}
+
+	protected String getStackTrace(Throwable aThrowable)
+	{
+	  final Writer result = new StringWriter();
+	  final PrintWriter printWriter = new PrintWriter(result);
+	  aThrowable.printStackTrace(printWriter);
+	  return result.toString();
+	}
+
+	protected void writeBasic(Map<String, Object> json, LoggingEvent event)
+	{
+	  json.put("hostName", getHost());
+	  json.put("timestamp", event.getTimeStamp());
+	  json.put("logger", event.getLoggerName());
+	  json.put("level", event.getLevel().toString());
+	  json.put("message", event.getMessage());
+	}
+
+	protected void writeThrowable(Map<String, Object> json, LoggingEvent event)
+	{
+	  final ThrowableInformation ti = event.getThrowableInformation();
+	  if (ti != null)
+	  {
+		final Throwable t = ti.getThrowable();
+
+		json.put("className", t.getClass().getCanonicalName());
+		json.put("stackTrace", getStackTrace(t));
+	  }
+	}
+  }
+
+  @Override
+  public void close()
+  {
+	client.close();
+	events.clear();
+	thread.isRunning = false;
+	semaphore.release();
+  }
+
+  @Override
+  public boolean requiresLayout()
+  {
+	return false;
+  }
+
+  @Override
+  protected void append(LoggingEvent event)
+  {
+	lock.lock();
+	try
+	{
+	  events.add(event);
+
+	  semaphore.release();
+	}
+	finally
+	{
+	  lock.unlock();
+	}
+  }
+
+  @Override
+  public void activateOptions()
+  {
+	super.activateOptions();
+
+	final Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", clusterName)
+	    .put("client.transport.ignore_cluster_name", "true").build();
+	client = new TransportClient().addTransportAddress(new InetSocketTransportAddress(host, port));
+
+	thread.start();
+  }
+
+  public String getHost()
+  {
+	return host;
+  }
+
+  public void setHost(String host)
+  {
+	this.host = host;
+  }
+
+  public int getPort()
+  {
+	return port;
+  }
+
+  public void setPort(int port)
+  {
+	this.port = port;
+  }
+
+  public String getClusterName()
+  {
+	return clusterName;
+  }
+
+  public void setClusterName(String clusterName)
+  {
+	this.clusterName = clusterName;
+  }
+
+  public String getIndex()
+  {
+	return index;
+  }
+
+  public void setIndex(String index)
+  {
+	this.index = index;
+  }
+
+  public String getType()
+  {
+	return type;
+  }
+
+  public void setType(String type)
+  {
+	this.type = type;
+  }
+
+  public void setQueuingWarningLevel(int queuingWarningLevel)
+  {
+	this.queuingWarningLevel = queuingWarningLevel;
+  }
+
+  @Override
+  public String toString()
+  {
+	return "ESAppender [host=" + host + ", port=" + port + ", clusterName=" + clusterName + ", index=" + index + ", type=" + type
+	    + ", queuingWarningLevel=" + queuingWarningLevel + "]";
+  }
+}
