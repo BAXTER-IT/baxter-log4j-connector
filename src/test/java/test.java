c22e374d93dd73784089ab2e241a90721d2fa82e
Peter Kormos <peter@baxter-it.com>
2014-12-09 10:06:08 +0100

    test class

---
+++ src/test/java/test.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.spi.LoggingEvent;
+
+import com.baxter.log4j.appender.impl.jest.ESAppender;
+
+public class test
+{
+  public static void main(String[] args) throws IOException, InterruptedException
+  {
+	final ESAppender es = new ESAppender();
+
+	es.setHost("172.16.1.11");
+	es.setPort(9300);
+	es.setClusterName("elasticsearch");
+	es.setIndex("setindex");
+	es.setType("setType");
+	es.setLayout(new PatternLayout("[%d{ISO8601}][%p] %c - %m%n"));
+
+	es.activateOptions();
+
+	es.doAppend(new LoggingEvent("", null, System.currentTimeMillis(), Level.TRACE, "message1", null, null, null, null, null));
+
+	Thread.sleep(1000);
+	es.close();
+  }
+}
